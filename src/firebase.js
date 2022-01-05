af9988370c6d7f16f25b999dc498150686145cf0
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-01-05 21:02:42 +0530

    preview

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBvM5ALkVo93Kb33uae2l30gL80cNfR2ug",
-	authDomain: "resume-353ce.firebaseapp.com",
-	projectId: "resume-353ce",
-	storageBucket: "resume-353ce.appspot.com",
-	messagingSenderId: "109862388331",
-	appId: "1:109862388331:web:aec5898c6f1dd57200672e",
+	apiKey: "AIzaSyAFfq8rOX9hpir5PMB21ZySg_O76MeAsHA",
+	authDomain: "resume-265ab.firebaseapp.com",
+	projectId: "resume-265ab",
+	storageBucket: "resume-265ab.appspot.com",
+	messagingSenderId: "131259358163",
+	appId: "1:131259358163:web:06ee0e17c844bf0f632d26",
 };
 
 firebase.initializeApp(firebaseConfig);
